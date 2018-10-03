1faa1db657d9c33511fe708c1fbcb85a98cc8d04
Sameer3079 <smrbasil4@gmail.com>
2018-10-03 08:42:49 +0530

    Partially implemented the employee add/update form

--- src/app/management/employee/employee.component.ts
+++ src/app/management/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import axios from 'axios'
 
 @Component({
   selector: 'app-employee',
@@ -9,13 +10,39 @@ import { ActivatedRoute } from '@angular/router'
 export class EmployeeComponent implements OnInit {
   empId: String
   private sub: any
+  employee = {
+    type: '',
+    firstName: '',
+    lastName: '',
+    address: '',
+    email: '',
+    contactNo: ''
+  }
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.empId = params['empId']
     })
-    alert(this.empId)
+
+    if (this.empId === undefined) {
+
+    } else {
+      axios.get('http://localhost:8093/employees/' + this.empId).then(response => {
+        this.employee.type = response.data.type
+        this.employee.firstName = response.data.firstName
+        this.employee.lastName = response.data.lastName
+        this.employee.address = response.data.address
+        this.employee.email = response.data.email
+        this.employee.contactNo = response.data.contactNo
+      })
+    }
+  }
+
+  addOrUpdateEmployee() {
+    axios.put('http://localhost:8093/employees', this.employee).then(response => {
+      alert(response.data.message)
+    })
   }
 
   ngOnDestroy() {
--- src/app/management/employee/employee.module.ts
+++ src/app/management/employee/employee.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { DndModule } from 'ng2-dnd';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { MatTableModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatGridListModule } from '@angular/material';
+import { MatTableModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatGridListModule, MatSelectModule, MatOptionModule } from '@angular/material';
 import {
     MatToolbarModule,
     MatListModule,
@@ -28,7 +28,9 @@ export const ROUTES: Routes = [
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
-        MatGridListModule
+        MatGridListModule,
+        MatSelectModule,
+        MatOptionModule
     ],
     declarations: [EmployeeComponent]
 })
