d7fbd98e71ba61975171a4624e8d85efd5dee4b7
sachithkk <tharakakk@gmail.com>
2018-10-05 20:36:16 +0530

    create get order component

---
+++ src/app/siteManager/item/get-order/get-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-item',
+  templateUrl: './get-item.component.html',
+  styleUrls: ['./get-item.component.scss']
+})
+export class GetItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
