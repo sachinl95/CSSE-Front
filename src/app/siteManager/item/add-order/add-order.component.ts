9989741525f311b968bf3c94f59f6efc07bc3ee6
sachithkk <tharakakk@gmail.com>
2018-10-05 20:42:34 +0530

    create add order component

---
+++ src/app/siteManager/item/add-order/add-order.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms';
+import { OrderServiceService } from '../../service/order-service.service';
+import { ItemModel } from '../../../model/item';
+import { MatTableDataSource, MatPaginator, MatSort}       from '@angular/material';
+import {orderModel} from '../../../model/order';
+
+@Component({
+  selector: 'app-add-order',
+  templateUrl: './add-order.component.html',
+  styleUrls: ['./add-order.component.scss']
+})
+export class AddOrderComponent implements OnInit {
+
+  orderForm:FormGroup;
+  order:orderModel;
+  item : ItemModel;
+
+  nameList : string[] = [];
+  
+
+  constructor(private orderService:OrderServiceService,private fb: FormBuilder) { 
+
+    this.orderForm = fb.group({  
+      'items' : [null, Validators.required],  
+      'quentity' : [null, Validators.required],  
+      'orderDate' : [null, Validators.required],  
+      'supplierName' : [null, Validators.required], 
+    });
+  }
+
+  dataSource = new MatTableDataSource();
+
+  ngOnInit() {
+
+    this.item = new ItemModel;
+    this.order = new orderModel;
+    //this.order = new PostItemModel;
+  
+    this.orderService.getItem().subscribe(
+      (data:any) => {
+          const Items: Item[] = data.message;
+          console.log(Items[1]);
+          
+          for(var i=0 ; i<Items.length ; i++){
+              this.nameList.push(Items[i].itemName);
+          }
+      }
+    )
+
+  }
+
+
+  onFormSubmit(form:NgForm)  {  
+    
+    if(this.orderForm.valid){
+      this.orderService.postOrder(form).subscribe((data:any) => {
+        console.log(data);
+      })
+    }
+  }
+
+
+}
+
+ 
+
+export interface Item {
+  id:string;
+  itemName:string;
+  categoryId:string;
+  price:string;
+  deliveryInformation:string;
+ // isRestricedItem:string;
+}
+
+
+
